b2739121b9d3c2bd42501fa0ee30da1afad47e84
Ermin Maksumić <56846473+ErminMaksumic@users.noreply.github.com>
2021-12-12 22:23:20 +0100

    Fix

---
+++ TravelPal/src/app/events/events-edit/events-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventsEditComponent } from './events-edit.component';
+
+describe('EventsEditComponent', () => {
+  let component: EventsEditComponent;
+  let fixture: ComponentFixture<EventsEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EventsEditComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EventsEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
